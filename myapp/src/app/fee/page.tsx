29f3ff68ed76f55edfc2b723ffb195e07f1b42a2
arfanbashir-dev <arfanbashir.dev@gmail.com>
2025-08-12 20:28:18 +0500

    Manages Images Folder

--- myapp/src/app/fee/page.tsx
+++ myapp/src/app/fee/page.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-import FeeImg from '../../../school pics/18.jpg'
-import MessImg from '../../../school pics/15.jpg'
-import InfoImg from '../../../school pics/19.jpg'
+import FeeImg from '../../../SchoolPics/18.jpg'
+import MessImg from '../../../SchoolPics/15.jpg'
+import InfoImg from '../../../ShoolPics/19.jpg'
 
 
 
