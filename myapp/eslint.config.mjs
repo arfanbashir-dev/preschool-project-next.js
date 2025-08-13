7c7e4251b4d57835c4a613bf33ad5bc861c7c66e
arfanbashir-dev <arfanbashir.dev@gmail.com>
2025-08-13 10:07:55 +0500

    ignore warning of image in eslint.config.mjs file

--- myapp/eslint.config.mjs
+++ myapp/eslint.config.mjs
@@ -11,11 +11,38 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // ✅ Add this object to target all TSX/JSX files
   {
+    files: ["**/*.{js,jsx,ts,tsx}"],
     rules: {
-      "@next/next/no-img-element": "off", // ✅ Disable <img> warning
+      "@next/next/no-img-element": "off",
     },
   },
 ];
 
 export default eslintConfig;
+
+
+
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//     rules: {
+//       "@next/next/no-img-element": "off", // ✅ Disable <img> warning
+//     },
+//   },
+// ];
+
+// export default eslintConfig;
