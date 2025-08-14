5141623b55f76ffeea99254a9ed167787fa3d975
arfanbashir-dev <arfanbashir.dev@gmail.com>
2025-08-14 09:37:09 +0500

    responsive settings

--- myapp/src/app/sports/page.tsx
+++ myapp/src/app/sports/page.tsx
@@ -16,7 +16,7 @@ export default function SportsFunc(){
             <div className="px-6 py-10 max-w-6xl mx-auto space-y-10">
             
                 <motion.h1  initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }}
-                    className="text-4xl font-bold  flex items-center gap-3">
+                    className="text-4xl font-bold text-yellow-500 flex items-center gap-3">
                     <GiRunningShoe className="text-dark text-5xl" />
                     Sports & Physical Development
                 </motion.h1>
