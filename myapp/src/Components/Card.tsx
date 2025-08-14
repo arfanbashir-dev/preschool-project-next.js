e4d5d997935276c9e55e5fa1a8e458b4e54b027e
arfanbashir-dev <arfanbashir.dev@gmail.com>
2025-08-14 07:20:04 +0500

    mix files

--- myapp/src/Components/Card.tsx
+++ myapp/src/Components/Card.tsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { ReactNode } from 'react';
 
-const Card = ({icon, title, value}) => {
-  return (
-    <div className='bg-medium  p-4 rounded-lg shadow-md flex items-center space-x-6 '>
-        <div className='text-3xl text-gray-500'>{icon}</div>
-        <div>
-            <h2 className='text-lg font-semibold'>{title}</h2>
-            <p className='text-xl'>{value}</p>
-        </div>
-    </div>
-  )
+interface CardProps { icon: ReactNode; // for things like <FaUserFriends />
+  title: string;
+  value: string | number;
 }
 
-export default Card
+const Card: React.FC<CardProps> = ({ icon, title, value }) => {
+  return (
+    <div className='bg-medium p-4 rounded-lg shadow-md flex items-center space-x-6'>
+      <div className='text-3xl text-gray-500'>{icon}</div>
+      <div>
+        <h2 className='text-lg font-semibold'>{title}</h2>
+        <p className='text-xl'>{value}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
+
+
+// import React from 'react'
+
+// const Card = ({icon, title, value}) => {
+//   return (
+//     <div className='bg-medium  p-4 rounded-lg shadow-md flex items-center space-x-6 '>
+//         <div className='text-3xl text-gray-500'>{icon}</div>
+//         <div>
+//             <h2 className='text-lg font-semibold'>{title}</h2>
+//             <p className='text-xl'>{value}</p>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Card
