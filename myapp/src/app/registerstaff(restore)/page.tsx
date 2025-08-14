06a360956a7d1c0f17bf5f368bd3508cf6137871
arfanbashir-dev <arfanbashir.dev@gmail.com>
2025-08-14 07:07:59 +0500

    mix files

--- myapp/src/app/registerstaff(restore)/page.tsx
+++ myapp/src/app/registerstaff(restore)/page.tsx
@@ -1,24 +1,39 @@
-
-// ✅ Remove "use client"
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
-import { redirect } from "next/navigation";
-import RegisterStaffForm from "./RegisteredStaffForm";
-
-export default async function RegisterPage() {
- 
-  const session = await getServerSession(authOptions);
-
-  if (!session || session.user.role !== "admin") {
-    redirect("/loginadmin");
-  }
-
+export default function Tesing(){
   return (
-    <div className="p-10">
-      <RegisterStaffForm />
+    <div>
+      <h1>Testing </h1>
     </div>
-  );
+  )
 }
+
+
+
+
+// // ✅ Remove "use client"
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "@/lib/auth";
+
+// import { redirect } from "next/navigation";
+// import RegisterStaffForm from "./RegisteredStaffForm";
+
+// export default async function RegisterPage() {
+ 
+//   const session = await getServerSession(authOptions);
+
+//   if (!session || session.user.role !== "admin") {
+//     redirect("/loginadmin");
+//   }
+
+//   return (
+//     <div className="p-10">
+//       <RegisterStaffForm />
+//     </div>
+//   );
+// }
+
+
+
+
 
 
 
