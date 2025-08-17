c7a4a1b3fa46abd2dab49e7ad8f2b540578d0507
arfanbashir-dev <arfanbashir.dev@gmail.com>
2025-08-17 13:07:10 +0500

    Register staff

--- myapp/src/app/registerstaff/page.tsx
+++ myapp/src/app/registerstaff/page.tsx
@@ -1,33 +1,26 @@
-export default function Testing(){
-  return(
-    <div>
-      <h1>Testing</h1>
-    </div>
-  )
-}
 
 
-// // ✅ Remove "use client"
-// import { getServerSession } from "next-auth";
-// import { authOptions } from "@/lib/auth";
+// ✅ Remove "use client"
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-// import { redirect } from "next/navigation";
-// import RegisterStaffForm from "./RegisteredStaffForm";
+import { redirect } from "next/navigation";
+import RegisterStaffForm from "./RegisteredStaffForm";
 
-// export default async function RegisterPage() {
+export default async function RegisterPage() {
  
-//   const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-//   if (!session || session.user.role !== "admin") {
-//     redirect("/loginadmin");
-//   }
+  if (!session || session.user.role !== "admin") {
+    redirect("/loginadmin");
+  }
 
-//   return (
-//     <div className="p-10">
-//       <RegisterStaffForm />
-//     </div>
-//   );
-// }
+  return (
+    <div className="p-10">
+      <RegisterStaffForm />
+    </div>
+  );
+}
 
 
 
